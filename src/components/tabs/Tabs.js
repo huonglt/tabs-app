bf2cd7a1aaf5465823a23cf6408b73fe5221b10d
helen.thu.huong <helen.thu.huong@luxuryescapes.com>
2022-03-07 10:11:48 +1300

    render tabpanel when a tab is selected

--- src/components/tabs/Tabs.js
+++ src/components/tabs/Tabs.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 import Tab from "./Tab";
+import TabPanel from "./TabPanel";
 import "./tabs.css";
 
 const Tabs = (props) => {
@@ -14,27 +15,26 @@ const Tabs = (props) => {
   };
 
   return (
-    <div role="tablist" aria-label="Sample tab list">
-      {children.map((child, index) => {
-        const childProps = child.props;
-        console.log(`childProps = ${JSON.stringify(childProps)}`);
-        console.log(
-          `child.type = ${child.type.name}, child.label = ${child.label}, child.value = ${child.value}`
-        );
-        if (child.type.name === "Tab") {
-          return (
-            <Tab
-              key={`tab-${index}`}
-              value={child.props.value}
-              label={child.props.label}
-              onClick={handleTabClick}
-              selected={child.props.id === activeTabId}
-            ></Tab>
+    <div role="tablist" aria-label="Sample tab list" className="tabs">
+      <div>
+        {children.map((child, index) => {
+          console.log(
+            `child.type = ${child.type.name}, child.label = ${child.label}, child.value = ${child.value}`
           );
-        }
-        return null;
-      })}
-
+          if (child.type.name === "Tab") {
+            return (
+              <Tab
+                key={`tab-${index}`}
+                value={child.props.value}
+                label={child.props.label}
+                onClick={handleTabClick}
+                selected={child.props.value === activeTabId}
+              ></Tab>
+            );
+          }
+          return null;
+        })}
+      </div>
       {/* Tab panel container */}
       <div>
         {children.map((child, index) => {
