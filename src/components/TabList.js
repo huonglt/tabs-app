788a200f46345d149a92e3800f718550f093945b
helen.thu.huong <helen.thu.huong@luxuryescapes.com>
2022-03-06 21:08:46 +1300

    Use Tab component

--- src/components/TabList.js
+++ src/components/TabList.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Tab from "./Tab";
 
 const TabList = (props) => {
   const { items } = props;
   return (
     <div role="tablist" aria-label="Sample tab list">
-      {items && items.map((item) => <div>{item}</div>)}
+      {items && items.map((item, key) => <Tab key={key}>{item}</Tab>)}
     </div>
   );
 };
