5e7f0370b9f275c6762e22963ad32b17aa7e93be
helen.thu.huong <helen.thu.huong@luxuryescapes.com>
2022-03-06 21:15:35 +1300

    use children instead

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,11 +1,10 @@
 import React from "react";
-import Tab from "./Tab";
 
 const Tabs = (props) => {
-  const { items } = props;
+  const { children } = props;
   return (
     <div role="tablist" aria-label="Sample tab list">
-      {items && items.map((item, key) => <Tab key={key}>{item}</Tab>)}
+      {children}
     </div>
   );
 };
