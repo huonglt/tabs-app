f486bd656954ebc81c509a6a43b7fa792a744054
helen.thu.huong <helen.thu.huong@luxuryescapes.com>
2022-03-06 21:16:02 +1300

    Show a simple Tabs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Tabs from "./components/Tabs";
+import Tab from "./components/Tab";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Tabs>
+        <Tab label="ITEM ONE" selected={false}></Tab>
+        <Tab label="ITEM TWO" selected={false}></Tab>
+        <Tab label="ITEM THREE" selected={false}></Tab>
+      </Tabs>
     </div>
   );
 }
