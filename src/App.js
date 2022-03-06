1bd3b9bb510f3caa45fd3a5052c322c3a281a5ad
helen.thu.huong <helen.thu.huong@luxuryescapes.com>
2022-03-06 22:23:44 +1300

    replace id with value for Tab 3

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <Tabs>
           <Tab label="ITEM ONE" value="1"></Tab>
           <Tab label="ITEM TWO" value="2"></Tab>
-          <Tab label="ITEM THREE" id="3"></Tab>
+          <Tab label="ITEM THREE" value="3"></Tab>
           <TabPanel value="1">Content of tab 1</TabPanel>
           <TabPanel value="2">Content of tab 2</TabPanel>
           <TabPanel value="3">Content of tab 3</TabPanel>
