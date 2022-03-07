4f4971e74338faf0bffda997cf83b7f4751e57d6
helen.thu.huong <helen.thu.huong@luxuryescapes.com>
2022-03-07 17:45:17 +1300

    Add event handler to handle left arrow key and right arrow key

--- src/components/tabs/Tabs.js
+++ src/components/tabs/Tabs.js
@@ -1,6 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Tab from "./Tab";
 import "./tabs.css";
+
+const LEFT_ARROW_KEY = 37;
+const RIGHT_ARROW_KEY = 39;
 
 const Tabs = (props) => {
   const { children, label } = props;
@@ -15,6 +18,29 @@ const Tabs = (props) => {
   };
 
   const ariaLabel = label ?? "A simple tab group";
+
+  /**
+   * Handle arrow key
+   */
+  const handleKeyUp = (event) => {
+    if (event.keyCode === LEFT_ARROW_KEY) {
+      console.log(`left arrow key pressed`);
+    } else if (event.keyCode === RIGHT_ARROW_KEY) {
+      console.log(`right arrow key pressed`);
+    }
+  };
+
+  /**
+   * Register event hanlder when component mounted
+   * Unregister event hander when component unmounted
+   */
+  useEffect(() => {
+    window.addEventListener("keyup", handleKeyUp);
+
+    return () => {
+      window.removeEventListener("keyup", handleKeyUp);
+    };
+  }, []);
 
   return (
     <div className="tabsGroup">
