4f8ec19bb0f0f99c20df8373ceac116f8605ac60
helen.thu.huong <helen.thu.huong@luxuryescapes.com>
2022-03-07 20:48:42 +1300

    Create simple tab group component

---
+++ src/components/sample-tab-groups/SimpleTabGroup.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Tabs from "../tabs/Tabs";
+import Tab from "../tabs/Tab";
+import TabPanel from "../tabs/TabPanel";
+
+const SimpleTabGroup = (props) => {
+  return (
+    <Tabs>
+      <Tab label="ITEM 1" value="1"></Tab>
+      <Tab label="ITEM 2" value="2"></Tab>
+      <Tab label="ITEM 3" value="3"></Tab>
+      <TabPanel value="1">
+        <p>Content of tab 1</p>
+      </TabPanel>
+      <TabPanel value="2">
+        <p>Content of tab 2</p>
+      </TabPanel>
+      <TabPanel value="3">
+        <p>Content of tab 3</p>
+      </TabPanel>
+    </Tabs>
+  );
+};
+
+export default SimpleTabGroup;
