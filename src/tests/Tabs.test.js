f9647b4ff6acb48a4225e66d1b64964b8c93f4aa
helen.thu.huong <helen.thu.huong@luxuryescapes.com>
2022-03-07 23:52:16 +1300

    unit test for SimpleTabGroup component

--- src/tests/Tabs.test.js
+++ src/tests/Tabs.test.js
@@ -1,17 +1,43 @@
 import { render, screen } from "@testing-library/react";
+import Tabs from "../components/tabs/Tab";
 import TabPanel from "../components/tabs/TabPanel";
-import Tabs from "../components/tabs/Tabs";
 import Tab from "../components/tabs/Tab";
+import SimpleTabGroup from "../components/sample/SimpleTabGroup";
 
-describe("TabGroup", () => {
-  it("render without problem", () => {
-    render(
-      <Tabs label="tabs">
-        <Tab label="Tab A" value="a" />
-        <TabPanel value="a">
-          <p>Tab Content</p>
-        </TabPanel>
-      </Tabs>
-    );
+describe("SimpleTabGroup", () => {
+  it("Accessibility Features correct", () => {
+    const { debug } = render(<SimpleTabGroup />);
+
+    const tabListElem = screen.getByRole("tablist");
+
+    expect(tabListElem).toBeInTheDocument();
+    expect(tabListElem.getAttribute("aria-label")).toEqual("simple-tab");
+
+    // check each tab
+    let tabs = screen.getAllByRole("tab");
+
+    // tab 0, selected
+    expect(tabs[0].getAttribute("value")).toEqual("1");
+    expect(tabs[0].getAttribute("aria-controls")).toEqual("1");
+    expect(tabs[0].getAttribute("tabindex")).toEqual("0");
+    expect(tabs[0].getAttribute("id")).toEqual("1");
+    expect(tabs[0].getAttribute("aria-selected")).toEqual("true");
+    expect(tabs[0].innerHTML).toEqual("ITEM 1");
+
+    // tab 1, not selected
+    expect(tabs[1].getAttribute("value")).toEqual("2");
+    expect(tabs[1].getAttribute("aria-controls")).toEqual("2");
+    expect(tabs[1].getAttribute("tabindex")).toEqual("-1");
+    expect(tabs[1].getAttribute("id")).toEqual("2");
+    expect(tabs[1].getAttribute("aria-selected")).toEqual("false");
+    expect(tabs[1].innerHTML).toEqual("ITEM 2");
+
+    // tab 2, not selected
+    expect(tabs[2].getAttribute("value")).toEqual("3");
+    expect(tabs[2].getAttribute("aria-controls")).toEqual("3");
+    expect(tabs[2].getAttribute("tabindex")).toEqual("-1");
+    expect(tabs[2].getAttribute("id")).toEqual("3");
+    expect(tabs[2].getAttribute("aria-selected")).toEqual("false");
+    expect(tabs[2].innerHTML).toEqual("ITEM 3");
   });
 });
