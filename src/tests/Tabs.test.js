cab875e2da2b347a24d9ab3f1ea7414fd45928b7
helen.thu.huong <helen.thu.huong@luxuryescapes.com>
2022-03-08 10:41:43 +1300

    Unit Test WAI ARIA spec is correct for TwoTabs component

---
+++ src/tests/Tabs.test.js
@@ -0,0 +1,34 @@
+import { TwoTabsGroup } from "./data";
+import { render, screen, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+describe("Tabs component", () => {
+  it(`WAI ARIA spec is correct for TwoTabs component`, () => {
+    render(<TwoTabsGroup />);
+
+    // role tablist exist
+    const tabListElem = screen.getByRole("tablist");
+
+    expect(tabListElem).toBeInTheDocument();
+    expect(tabListElem.getAttribute("aria-label")).toEqual("two-tabs-group");
+
+    // check each tab via role tab
+    let tabs = screen.getAllByRole("tab");
+
+    // tab 0, selected, value, aria-controls, tabindex, id, aria-selected, and innerHTML correct
+    expect(tabs[0].getAttribute("value")).toEqual("tab1");
+    expect(tabs[0].getAttribute("aria-controls")).toEqual("tab1");
+    expect(tabs[0].getAttribute("tabindex")).toEqual("0");
+    expect(tabs[0].getAttribute("id")).toEqual("tab1");
+    expect(tabs[0].getAttribute("aria-selected")).toEqual("true");
+    expect(tabs[0].innerHTML).toEqual("Tab 1");
+
+    // tab 1, not selected, value, aria-controls, tabindex, id, aria-selected, and innerHTML correct
+    expect(tabs[1].getAttribute("value")).toEqual("tab2");
+    expect(tabs[1].getAttribute("aria-controls")).toEqual("tab2");
+    expect(tabs[1].getAttribute("tabindex")).toEqual("-1");
+    expect(tabs[1].getAttribute("id")).toEqual("tab2");
+    expect(tabs[1].getAttribute("aria-selected")).toEqual("false");
+    expect(tabs[1].innerHTML).toEqual("Tab 2");
+  });
+});
