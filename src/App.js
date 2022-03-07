00cd3ff4c2e77e5dc5655a70a2f33dd5fb7c5ff6
helen.thu.huong <helen.thu.huong@luxuryescapes.com>
2022-03-07 21:30:28 +1300

    Initial setup for the demo page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import "./App.css";
-import React, { useRef } from "react";
+import React, { useState } from "react";
 import SimpleTabGroup from "./components/sample/SimpleTabGroup";
 
 function App() {
+  const [sample, setSample] = useState("simple");
+
+  const simpleSampleClick = () => setSample("simple");
+
   return (
     <div className="App">
       <div className="appContainer">
         <div className="leftPane">
-          <div>Simple Tab Groups</div>
+          <div onClick={simpleSampleClick}>Simple Tab Groups</div>
           <div>Multi Tab Groups</div>
           <div>Tab Groups with Link to tab</div>
         </div>
-        <div className="rightPane">right pane</div>
+        <div className="rightPane">
+          {sample === "simple" && <SimpleTabGroup />}
+        </div>
       </div>
     </div>
   );
