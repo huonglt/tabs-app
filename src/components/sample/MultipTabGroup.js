5358dfb973c4375aa6fbc666d7f7e7b3f6674229
helen.thu.huong <helen.thu.huong@luxuryescapes.com>
2022-03-07 22:14:32 +1300

    Sample of multi tab groups

--- src/components/sample/MultipTabGroup.js
+++ src/components/sample/MultipTabGroup.js
@@ -6,41 +6,45 @@ import TabPanel from "../tabs/TabPanel";
 const MultiTabGroup = (props) => {
   return (
     <div>
-      <Tabs>
-        <Tab label="GROUP A - ITEM 1" value="tabGroupA-1"></Tab>
-        <Tab label="GROUP A - ITEM 2" value="tabGroupA-2"></Tab>
-        <Tab label="GROUP A - ITEM 3" value="tabGroupA-3"></Tab>
-        <TabPanel value="tabGroupA-1">
-          <p>Tab Group A</p>
-          <p>Content of Item 1</p>
-        </TabPanel>
-        <TabPanel value="tabGroupA-2">
-          <p>Tab Group A</p>
-          <p>Content of Item 2</p>
-        </TabPanel>
-        <TabPanel value="tabGroupA-3">
-          <p>Tab Group A</p>
-          <p>Content of Item 3</p>
-        </TabPanel>
-      </Tabs>
+      <div className="tabGroupContainer">
+        <Tabs label="tab-group-A">
+          <Tab label="GROUP A - ITEM 1" value="tabGroupA-1"></Tab>
+          <Tab label="GROUP A - ITEM 2" value="tabGroupA-2"></Tab>
+          <Tab label="GROUP A - ITEM 3" value="tabGroupA-3"></Tab>
+          <TabPanel value="tabGroupA-1">
+            <p>Tab Group A</p>
+            <p>Content of Item 1</p>
+          </TabPanel>
+          <TabPanel value="tabGroupA-2">
+            <p>Tab Group A</p>
+            <p>Content of Item 2</p>
+          </TabPanel>
+          <TabPanel value="tabGroupA-3">
+            <p>Tab Group A</p>
+            <p>Content of Item 3</p>
+          </TabPanel>
+        </Tabs>
+      </div>
 
-      <Tabs>
-        <Tab label="GROUP B ITEM 1" value="tabGroupB-1"></Tab>
-        <Tab label="GROUP B ITEM 2" value="tabGroupB-2"></Tab>
-        <Tab label="GROUP B ITEM 3" value="tabGrouoB-3"></Tab>
-        <TabPanel value="tabGroupB-1">
-          <p>Tab Group B</p>
-          <p>Content of Item 1</p>
-        </TabPanel>
-        <TabPanel value="tabGroupB-2">
-          <p>Tab Group B</p>
-          <p>Content of Item 2</p>
-        </TabPanel>
-        <TabPanel value="tabGroupB-3">
-          <p>Tab Group B</p>
-          <p>Content of Item 3</p>
-        </TabPanel>
-      </Tabs>
+      <div className="tabGroupContainer">
+        <Tabs label="tab-group-B">
+          <Tab label="GROUP B ITEM 1" value="tabGroupB-1"></Tab>
+          <Tab label="GROUP B ITEM 2" value="tabGroupB-2"></Tab>
+          <Tab label="GROUP B ITEM 3" value="tabGroupB-3"></Tab>
+          <TabPanel value="tabGroupB-1">
+            <p>Tab Group B</p>
+            <p>Content of Item 1</p>
+          </TabPanel>
+          <TabPanel value="tabGroupB-2">
+            <p>Tab Group B</p>
+            <p>Content of Item 2</p>
+          </TabPanel>
+          <TabPanel value="tabGroupB-3">
+            <p>Tab Group B</p>
+            <p>Content of Item 3</p>
+          </TabPanel>
+        </Tabs>
+      </div>
     </div>
   );
 };
