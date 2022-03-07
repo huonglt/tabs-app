edd0cc0d09c5206ca87e4478f25ce005261d0950
helen.thu.huong <helen.thu.huong@luxuryescapes.com>
2022-03-07 21:43:07 +1300

    Show multi tab groups sampe

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import "./App.css";
 import React, { useState } from "react";
 import SimpleTabGroup from "./components/sample/SimpleTabGroup";
+import MultiTabGroup from "./components/sample/MultipTabGroup";
 
 function App() {
   const [sample, setSample] = useState("simple");
 
   const simpleSampleClick = () => setSample("simple");
+  const multiSampleClick = () => setSample("multi");
 
   return (
     <div className="App">
       <div className="appContainer">
         <div className="leftPane">
           <div onClick={simpleSampleClick}>Simple Tab Groups</div>
-          <div>Multi Tab Groups</div>
+          <div onClick={multiSampleClick}>Multi Tab Groups</div>
           <div>Tab Groups with Link to tab</div>
         </div>
         <div className="rightPane">
           {sample === "simple" && <SimpleTabGroup />}
+          {sample === "multi" && <MultiTabGroup />}
         </div>
       </div>
     </div>
