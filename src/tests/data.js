ea1200c6b6d34e467aea81c87832cdfc42884ce7
helen.thu.huong <helen.thu.huong@luxuryescapes.com>
2022-03-08 10:41:26 +1300

    Add data for unit test

---
+++ src/tests/data.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Tabs from "../components/tabs/Tabs";
+import Tab from "../components/tabs/Tab";
+import TabPanel from "../components/tabs/TabPanel";
+
+export const TwoTabsGroup = (props) => {
+  return (
+    <Tabs label="two-tabs-group">
+      <Tab label="Tab 1" value="tab1">
+        Tab 1
+      </Tab>
+      <Tab label="Tab 2" value="tab2">
+        Tab 2
+      </Tab>
+      <TabPanel value="tab1">Tab 1 content</TabPanel>
+      <TabPanel value="tab2">Tab 2 content</TabPanel>
+    </Tabs>
+  );
+};
+
+export const NoTabPanel = (props) => {
+  return (
+    <Tabs>
+      <Tab label="Tab 1"></Tab>
+      <Tab label="Tab 2"></Tab>
+    </Tabs>
+  );
+};
+
+export const NoTab = (props) => {
+  return (
+    <Tabs>
+      <Tab label="Tab 1"></Tab>
+      <Tab label="Tab 2"></Tab>
+    </Tabs>
+  );
+};
