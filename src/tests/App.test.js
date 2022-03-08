7c93f3b8a294965bb5f8d5df1e5cf835dee8d9a4
helen.thu.huong <helen.thu.huong@luxuryescapes.com>
2022-03-08 13:53:12 +1300

    right panel will show simple tab group example

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -9,4 +9,13 @@ describe("App component", () => {
     expect(screen.getByText("Multi Tab Groups")).toBeInTheDocument();
     expect(screen.getByText("Tab Group With Link To Tab")).toBeInTheDocument();
   });
+
+  it("right panel will show simple tab group example", () => {
+    render(<App />);
+    expect(screen.getByText("Simple Tab Group Example")).toBeInTheDocument();
+    expect(screen.getByText("ITEM 1")).toBeInTheDocument();
+    expect(screen.getByText("ITEM 2")).toBeInTheDocument();
+    expect(screen.getByText("ITEM 3")).toBeInTheDocument();
+    expect(screen.getByText("Content of tab 1")).toBeInTheDocument();
+  });
 });
