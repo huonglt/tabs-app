2a5980b5fa2d80d9216ea493d60f05ae40765b78
helen.thu.huong <helen.thu.huong@luxuryescapes.com>
2022-03-08 13:55:34 +1300

    unit test click Multi Tab Groups option, multi tab groups will show

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -18,4 +18,26 @@ describe("App component", () => {
     expect(screen.getByText("ITEM 3")).toBeInTheDocument();
     expect(screen.getByText("Content of tab 1")).toBeInTheDocument();
   });
+
+  it("click Multi Tab Groups option, multi tab groups will show", () => {
+    render(<App />);
+    fireEvent.click(screen.getByText("Multi Tab Groups"));
+    expect(screen.getByText("Multi Tab Groups Example")).toBeInTheDocument();
+
+    expect(screen.getByText("Multi Tab Groups Example")).toBeInTheDocument();
+
+    // tab group A shown correctly
+    expect(screen.getByText("GROUP A - ITEM 1")).toBeInTheDocument();
+    expect(screen.getByText("GROUP A - ITEM 2")).toBeInTheDocument();
+    expect(screen.getByText("GROUP A - ITEM 3")).toBeInTheDocument();
+    expect(screen.getByText("Tab Group A")).toBeInTheDocument();
+    expect(screen.getByText("Content of Item 1")).toBeInTheDocument();
+
+    // tab group B shown correctly
+    expect(screen.getByText("GROUP B - TAB 1")).toBeInTheDocument();
+    expect(screen.getByText("GROUP B - TAB 2")).toBeInTheDocument();
+    expect(screen.getByText("GROUP B - TAB 3")).toBeInTheDocument();
+    expect(screen.getByText("Tab Group B")).toBeInTheDocument();
+    expect(screen.getByText("Content of Tab 1")).toBeInTheDocument();
+  });
 });
